ac0952987b3573a84cdb1ce9d36321518b1e3e5a
Evan Lockwood <elockwood@olin.edu>
2024-10-02 14:30:48 -0400

    Add cartesian_waypoint_pick_up_example

---
+++ api_python/examples/110-Waypoints/cartesian_waypoint_pick_up_example.py
@@ -0,0 +1,216 @@
+#! /usr/bin/env python3
+
+###
+# KINOVA (R) KORTEX (TM)
+#
+# Copyright (c) 2021 Kinova inc. All rights reserved.
+#
+# This software may be modified and distributed
+# under the terms of the BSD 3-Clause license.
+#
+# Refer to the LICENSE file for details.
+#
+###
+
+import sys
+import os
+import time
+import threading
+
+from kortex_api.autogen.client_stubs.BaseClientRpc import BaseClient
+from kortex_api.autogen.client_stubs.BaseCyclicClientRpc import BaseCyclicClient
+
+
+from kortex_api.autogen.messages import Base_pb2, BaseCyclic_pb2, Common_pb2
+
+# Maximum allowed waiting time during actions (in seconds)
+TIMEOUT_DURATION = 30
+GRIPPER_TIMEOUT_DURATION = 2
+
+# Create closure to set an event after an END or an ABORT
+def check_for_end_or_abort(e):
+    """Return a closure checking for END or ABORT notifications
+
+    Arguments:
+    e -- event to signal when the action is completed
+        (will be set when an END or ABORT occurs)
+    """
+    def check(notification, e = e):
+        print("EVENT : " + \
+              Base_pb2.ActionEvent.Name(notification.action_event))
+        if notification.action_event == Base_pb2.ACTION_END \
+        or notification.action_event == Base_pb2.ACTION_ABORT:
+            e.set()
+    return check
+ 
+def example_move_to_home_position(base):
+    # Make sure the arm is in Single Level Servoing mode
+    base_servo_mode = Base_pb2.ServoingModeInformation()
+    base_servo_mode.servoing_mode = Base_pb2.SINGLE_LEVEL_SERVOING
+    base.SetServoingMode(base_servo_mode)
+    
+    # Move arm to ready position
+    print("Moving the arm to a safe position")
+    action_type = Base_pb2.RequestedActionType()
+    action_type.action_type = Base_pb2.REACH_JOINT_ANGLES
+    action_list = base.ReadAllActions(action_type)
+    action_handle = None
+    for action in action_list.action_list:
+        if action.name == "Home":
+            action_handle = action.handle
+
+    if action_handle == None:
+        print("Can't reach safe position. Exiting")
+        return False
+
+    e = threading.Event()
+    notification_handle = base.OnNotificationActionTopic(
+        check_for_end_or_abort(e),
+        Base_pb2.NotificationOptions()
+    )
+
+    base.ExecuteActionFromReference(action_handle)
+    finished = e.wait(TIMEOUT_DURATION)
+    base.Unsubscribe(notification_handle)
+
+    if finished:
+        print("Safe position reached")
+    else:
+        print("Timeout on action notification wait")
+    return finished
+def populateCartesianCoordinate(waypointInformation):
+    
+    waypoint = Base_pb2.CartesianWaypoint()  
+    waypoint.pose.x = waypointInformation[0]
+    waypoint.pose.y = waypointInformation[1]
+    waypoint.pose.z = waypointInformation[2]
+    waypoint.blending_radius = waypointInformation[3]
+    waypoint.pose.theta_x = waypointInformation[4]
+    waypoint.pose.theta_y = waypointInformation[5]
+    waypoint.pose.theta_z = waypointInformation[6] 
+    waypoint.reference_frame = Base_pb2.CARTESIAN_REFERENCE_FRAME_BASE
+    
+    return waypoint
+
+def example_trajectory(base, base_cyclic):
+
+    base_servo_mode = Base_pb2.ServoingModeInformation()
+    base_servo_mode.servoing_mode = Base_pb2.SINGLE_LEVEL_SERVOING
+    base.SetServoingMode(base_servo_mode)
+    product = base.GetProductConfiguration()
+    waypointsDefinition = tuple(tuple())
+    if(product.model == Base_pb2.ProductConfiguration__pb2.MODEL_ID_L53 ):
+   
+        waypointsDefinition = ( (0.58,   0.03,  0.42,  0.0, 92.5, 1.5, 94.0),
+                                (0.57,  -0.07,  0.32, 0.1, 108.8, 0.8, 84.6),
+                                (0.5,   -0.24, 0.22, 0.1, 125.4, 0.2, 66.2),
+                                (0.4,  -0.32, 0.15,  0.1, 141.3, 0.0, 52.0),
+                                (0.19,   -0.37, 0.07, 0.1, 161.9, 0.5, 29.0),
+                                (0.1,   -0.35, 0.05, 0.1, 161.9, 0.5, 29.0),
+                                (0.075, -0.34, 0.033, 0.0, 178.5, 1.5, 15))
+    
+    else:
+        print("Product is not compatible to run this example please contact support with KIN number bellow")
+        print("Product KIN is : " + product.kin())
+
+    
+    waypoints = Base_pb2.WaypointList()
+    
+    waypoints.duration = 0.0
+    waypoints.use_optimal_blending = True
+    
+    index = 0
+    for waypointDefinition in waypointsDefinition:
+        waypoint = waypoints.waypoints.add()
+        waypoint.name = "waypoint_" + str(index)   
+        waypoint.cartesian_waypoint.CopyFrom(populateCartesianCoordinate(waypointDefinition))
+        index = index + 1 
+
+    # Verify validity of waypoints
+    result = base.ValidateWaypointList(waypoints)
+    if(len(result.trajectory_error_report.trajectory_error_elements) == 0):
+        e = threading.Event()
+        notification_handle = base.OnNotificationActionTopic(   check_for_end_or_abort(e),
+                                                                Base_pb2.NotificationOptions())
+
+        print("Moving cartesian trajectory...")
+        
+        base.ExecuteWaypointTrajectory(waypoints)
+
+        print("Waiting for trajectory to finish ...")
+        finished = e.wait(TIMEOUT_DURATION)
+        base.Unsubscribe(notification_handle)
+
+        return finished
+        
+    else:
+        print("Error found in trajectory") 
+        result.trajectory_error_report.PrintDebugString()
+
+def gripper_close(base):
+    gripper_command = Base_pb2.GripperCommand()
+    finger = gripper_command.gripper.finger.add()
+
+    e = threading.Event()
+    notification_handle = base.OnNotificationActionTopic(   check_for_end_or_abort(e),
+                                                                Base_pb2.NotificationOptions())
+
+    # Set position to close gripper
+    print ("Closing gripper using position command...")
+    gripper_command.mode = Base_pb2.GRIPPER_POSITION
+    finger.value = 0.57
+    base.SendGripperCommand(gripper_command)
+    
+    finished = e.wait(GRIPPER_TIMEOUT_DURATION)
+    base.Unsubscribe(notification_handle)
+    return finished
+
+def gripper_open(base):
+    gripper_command = Base_pb2.GripperCommand()
+    finger = gripper_command.gripper.finger.add()
+
+    e = threading.Event()
+    notification_handle = base.OnNotificationActionTopic(   check_for_end_or_abort(e),
+                                                                Base_pb2.NotificationOptions())
+
+    # Set position to close gripper
+    print ("Closing gripper using position command...")
+    gripper_command.mode = Base_pb2.GRIPPER_POSITION
+    finger.value = 0.00
+    base.SendGripperCommand(gripper_command)
+    
+    finished = e.wait(GRIPPER_TIMEOUT_DURATION)
+    base.Unsubscribe(notification_handle)
+    return finished
+
+
+def main():
+    
+    # Import the utilities helper module
+    sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+    import utilities
+
+    # Parse arguments
+    args = utilities.parseConnectionArguments()
+    
+    # Create connection to the device and get the router
+    with utilities.DeviceConnection.createTcpConnection(args) as router:
+
+        # Create required services
+        base = BaseClient(router)
+        base_cyclic = BaseCyclicClient(router)
+        
+
+        # Example core
+        success = True
+
+        success &= example_move_to_home_position(base)
+        success &= example_trajectory(base, base_cyclic)
+        success &= gripper_close(base)
+        success &= example_move_to_home_position(base)
+        success &= gripper_open(base)
+       
+        return 0 if success else 1
+
+if __name__ == "__main__":
+    exit(main())
